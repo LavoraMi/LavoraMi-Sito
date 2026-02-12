113d94d4c553886f7b26696d8ab443beec9a37b0
Andrea Filice <filiceandrea8@gmail.com>
2026-02-12 11:20:08 +0100

    fix: fixing some bugs
    * performance improvements
    * fixed some Themes Bugs

--- js/main.js
+++ js/main.js
@@ -15,6 +15,20 @@ const themeToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const body = document.body;
 
+///In this section, we check the favorite Theme settings by the User Settings.
+//?REFACTOR: Refactor this function to be only one function and be more clean.
+if(localStorage.getItem("theme") == null){
+    const darkModeMql = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
+    if (darkModeMql && darkModeMql.matches) {
+        themeIcon.classList.replace('bi-moon-stars-fill', 'bi-sun-fill');
+        localStorage.setItem('theme', 'dark');
+    } 
+    else {
+        themeIcon.classList.replace('bi-sun-fill', 'bi-moon-stars-fill');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
 ///We use the localStorage to save the Theme selected from the user, after that, we edit the theme based from this value.
 //?REFACTOR: Refactor this function to be only one function and be more clean.
 if (localStorage.getItem('theme') === 'light') {
