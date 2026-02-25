36e376884a68bc16d4acd2b270c1938e0282c0f8
Andrea Filice <filiceandrea8@gmail.com>
2026-02-25 22:58:39 +0100

    fix: fixing bugs
    * trying to fix SupabaseConfigurations

--- build-env.js
+++ build-env.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const configContent = `const ENV = {
+const configContent = `window.ENV = {
     SUPABASE_URL: "${process.env.SUPABASE_URL || ''}",
     SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY || ''}"
 };`;
