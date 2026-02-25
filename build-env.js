0af44a0dcd55a04da538095f5790a8d960c38732
Andrea Filice <filiceandrea8@gmail.com>
2026-02-25 20:16:19 +0100

    fix: fixing build errors

---
+++ build-env.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const configContent = `const ENV = {
+    SUPABASE_URL: "${process.env.SUPABASE_URL || ''}",
+    SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY || ''}"
+};`;
+
+fs.writeFileSync('./config.js', configContent);
+console.log("File config.js generato con successo per LavoraMI!");
