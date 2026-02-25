14b69eaadfc7fdbe91686fef50daaf80b368585d
Andrea Filice <filiceandrea8@gmail.com>
2026-02-25 22:09:42 +0100

    chore: reverting code

--- build-env.js
+++ build-env.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const configContent = `window.ENV = {
+const configContent = `const ENV = {
     SUPABASE_URL: "${process.env.SUPABASE_URL || ''}",
-    SUPABASE_KEY: "${process.env.SUPABASE_KEY || process.env.SUPABASE_ANON_KEY || ''}"
+    SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY || ''}"
 };`;
 
 fs.writeFileSync('./config.js', configContent);
