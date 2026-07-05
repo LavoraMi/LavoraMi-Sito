5c144486f87cf727ba0f2503ddfb9a66016355dd
Andrea Filice <filiceandrea8@gmail.com>
2026-07-05 20:58:58 +0200

    chore: removed brevo stuffs
    * removed old stuffs not used anymore

--- netlify/functions/get-secret.js
+++ netlify/functions/get-secret.js
@@ -2,7 +2,6 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({ 
-      BrevoAPIKEY: process.env.BREVO_API_KEY,
       SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY
     }),
   };
